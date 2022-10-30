fbdc5a9bacfa96708ad4dd244970e9ff142e6cbc
DoZerrro <ehdud1515@naver.com>
2022-10-30 23:17:07 +0900

    todo paging ing..(button 및 url 미완료)

--- src/components/Todo/TodoPaging.js
+++ src/components/Todo/TodoPaging.js
@@ -8,29 +8,35 @@ const todoPageList = async ({ page, size }) => {
 };
 
 const initState = {
-  start: 1,
+  dtoList: [],
   end: 10,
+  next: true,
+  pageRequestDTO: {
+    page: 1, size: 10, skip: 0
+  },
   prev: false,
-  next: false,
-  total: 0,
-  todoList: []
+  start: 1,
+  total: 0
 };
 
 function TodoPaging(props) {
   const [list, setList] = useState(initState);
-  const page = 2;
+  const page = 1;
   const size = 10;
+
   useEffect(() => {
     todoPageList({ page, size }).then((data) => {
       console.log("data-------", data);
-      setList(data);
-      console.log("list--------", list);
+      setList({ ...data });
     });
   }, []);
 
   return (
-    <div>
-    </div>
+    <>
+      <div>
+        {list.dtoList.map(({ tno, title }) => <li key={tno}>{title}</li>)}
+      </div>
+    </>
   );
 }
 
